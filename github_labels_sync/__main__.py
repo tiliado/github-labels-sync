80c5d5a133ec5317b4f6a0a27f26c48d766372c5
Jiří Janoušek <janousek.jiri@gmail.com>
2018-06-28 16:38:23 +0200

    Fix argv[0] for python3 -m
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- github_labels_sync/__main__.py
+++ github_labels_sync/__main__.py
@@ -7,9 +7,9 @@ from github_labels_sync import cli
 
 if __name__ == '__main__':
     argv = sys.argv[:]
-    argv[0] = 'python3 -m github_label_sync'
+    argv[0] = 'gh-label-sync'
     try:
-        sys.exit(cli.main(sys.argv) or 0)
+        sys.exit(cli.main(argv) or 0)
     except ValueError as e:
         print(e, file=sys.stderr)
         sys.exit(1)
